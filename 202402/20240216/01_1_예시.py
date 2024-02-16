93286b3432fa5f13ae553cfd71dc99b1ce4dd2e4
qwert0175 <suin990209@gmail.com>
2024-02-16 15:34:14 +0900

    update

--- 202402/20240216/01_1_예시.py
+++ 202402/20240216/01_1_예시.py
@@ -1,14 +1,8 @@
 # swea 노드의 거리
 
-'''
-V, E : 1~V번까지 V개의 정점, E개의 간선
-7 8
-1 2 1 3 2 4 2 5 4 6 5 6 6 7 3 7
-'''
-
 def bfs(s, V, G):  # 시작정점 s, 노드개수 N
     q = []
-    visited = [0] * (N+1)
+    visited = [0] * (V+1)
     q.append(s)
     visited[s] = 1
     while q:
